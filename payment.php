3facf8c2f84dafec0916653d2e0264a166b60a7f
Lakshya Khera <lakshya.khera@gmail.com>
2018-07-03 14:07:45 +0530

    Add files via upload

---
+++ payment.php
@@ -0,0 +1,86 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+	<title>Payment Info</title>
+</head>
+<body style="background-image:linear-gradient(rgba(0,0,0,0.3),rgba(0,0,0,0.3)),url(img/bg.jpg);background-size: cover;">
+	<a href="homepage.php">
+	<button style="position: absolute;top:5px;left: 2px">Back</button></a>
+	<center>
+		<div class="lead" style="background-color:  #9E9E9E;height: 40px;padding-top: 6px;box-shadow: 0px 2px  lightgrey">
+			<span style="font-weight: 500;color: white">PAYMENT INFO</span>
+		</div>
+	</center>
+	<a href="index.php">
+	<button style="position: absolute;top:5px;right: 2px">Logout</button></a>
+	<div style="margin-top: 60px;">
+		<span class="container"><b>Bank Account Details</b></span><hr>
+	<table class="table table-borderless">
+		  <thead>
+		    <tr>
+		      <th scope="col">Account Name</th>
+		      <td>SHREE GLOBAL HOLIDAYS INDIA PRIVATE LIMITED</td>
+		    </tr>
+		  </thead>
+		  <tbody>
+		    <tr>
+		      <th scope="row">Bank Name</th>
+		      <td>HDFC BANK LIMITED</td>
+		    </tr>
+		    <tr>
+		      <th scope="row">Branch Name</th>
+		      <td>Vishal Khand Near Manoj Pandey Chauraha Gomti Nagar</td>
+		    </tr>
+		    <tr>
+		      <th scope="row">Account Number</th>
+		      <td colspan="2">50200021711986</td>
+		    </tr>
+		    <tr>
+		      <th scope="row">IFS Code</th>
+		      <td colspan="2">HDFC0001112</td>
+		    </tr>
+		  </tbody>
+		</table>
+	</div>
+	<hr>
+	<div class="container" >
+		<div class="list-group" >
+  <div class="list-group-item list-group-item-action flex-column align-items-start">
+    <div class="d-flex w-100 justify-content-between">
+      <h5 class="mb-1">Email Address</h5>
+  	</div>
+  	<span>shreeglobalholidays@gmail.com</span>
+    </div>  
+    <div class="list-group-item list-group-item-action flex-column align-items-start">
+    <div class="d-flex w-100 justify-content-between">
+      <h5 class="mb-1">Address</h5>
+  </div>
+  <span>Shree Global holidays India Pvt Ltd. 1/687-Vishal Khand, Near CMS, Gomtinagar, Lucknow-UP, Pincode 226010</span>
+    </div>
+  <div class="list-group-item list-group-item-action flex-column align-items-start">
+    <div class="d-flex w-100 justify-content-between">
+      <h5 class="mb-1">Phone</h5>
+  </div>
+  <span>0522-4002830</span><br>
+  <span>+91 8400 420 088</span>
+    </div>
+</div>
+	<div style="padding-top: 10px;position: absolute;background-color:#6F3131;width:100%;height: 20%;;left: 0;margin-top: 20px;color: white;font-weight: 400">
+		<center>
+			&#169; Copyright Shree Global 
+		</center>
+	</div>
+	</div>
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+</body>
+</html>
